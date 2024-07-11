0b751c96c54587ac7bba40474609547ef3c2cb9f
Max Catman <xorgnak@gmail.com>
2024-07-11 14:04:18 -0600

    Create nomad.sh

---
+++ nomad.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/xorgnak/llamafile
+
+git clone --depth=1 https://github.com/xorgnak/remind
+
+git clone --depth=1 https://github.com/xorgnak/meiou
+
+git clone --depth=1 https://github.com/xorgnak/cardtrick
+
+git clone --depth=1 https://github.com/xorgnak/fortunecookie
+
+git clone --depth=1 https://github.com/xorgnak/maiden
+
+git clone --depth=1 https://github.com/xorgnak/pvector
+
+echo "[NOMAD] cloned binary utility sources."
