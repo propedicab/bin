fb7257d09eea5a5906b4892c4e86c405a783757a
Max Catman <xorgnak@gmail.com>
2024-07-11 14:42:08 -0600

    Update nomad.sh

--- nomad.sh
+++ nomad.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/xorgnak/llamafile
-
-git clone --depth=1 https://github.com/xorgnak/remind
-
-git clone --depth=1 https://github.com/xorgnak/meiou
-
-git clone --depth=1 https://github.com/xorgnak/cardtrick
-
-git clone --depth=1 https://github.com/xorgnak/fortunecookie
-
-git clone --depth=1 https://github.com/xorgnak/maiden
-
 git clone --depth=1 https://github.com/xorgnak/pvector
 
-echo "[NOMAD] cloned binary utility sources."
+sudo gem install llamafile iww remind meiou cardtrick fortunecookie maiden
+
+echo "[NOMAD] installed utilities."
