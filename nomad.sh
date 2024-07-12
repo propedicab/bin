43a42b0ec653d320e7043f2285beb84d2ab322a2
Max Catman <xorgnak@gmail.com>
2024-07-11 18:35:00 -0600

    Update nomad.sh

--- nomad.sh
+++ nomad.sh
@@ -2,6 +2,8 @@
 
 git clone --depth=1 https://github.com/xorgnak/pvector
 
+mkdir pvector/rem
+
 sudo gem install llamafile iww gnu-remind meiou cardtrick fortunecookie maiden
 
 echo "[NOMAD] installed utilities."
